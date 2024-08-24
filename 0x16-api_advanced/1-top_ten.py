6a1ec89d3c5afb9c1517075ffdac254320c58125
rahmoncodes <abdulrahmonbb@gmail.com>
2024-08-24 15:16:05 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,19 @@ import requests
 import sys
 
 
-def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
+def top_ten(subreddit):
+    """Read reddit API and return top 10 hotspots """
     username = 'ledbag123'
     password = 'Reddit72'
     user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
     headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     client = requests.session()
     client.headers = headers
     r = client.get(url, allow_redirects=False)
     if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
+        list_titles = r.json()['data']['children']
+        for a in list_titles[:10]:
+            print(a['data']['title'])
     else:
-        return(0)
+        return(print("None"))
